dcfb3bdcce8b923de9a52818a9457731712d03d1
NEHAL KHAN <khan.nihal200@gmail.com>
2026-03-25 07:27:36 +0000

    data-summary

--- .devcontainer/basics/comparison.js
+++ .devcontainer/basics/comparison.js
@@ -6,8 +6,16 @@
 
 //Above are normal usage so no need to see those much
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // ***comparison operator works different ***
 
-    
+// *****
+
+// console.log(undefined >= 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+// ===  Always avoid these comparison above
+
+
---
+++ .devcontainer/basics/datatypes-summary.js
@@ -0,0 +1,29 @@
+// Primitve
+
+// -- 7 types : 
+
+/*
+String -- const score = "100"
+Number -- const scoreVal = 100.3
+Boolean -- const isLoogedIn = false
+null -- const Out = null
+undefined --  let username;
+Symbol  -- const id = Symbol("123")
+BigInt  -- const bigNum = 15164543154646613n
+*/
+
+//Reference Types : Non-primitive
+
+/* Array , Objects , Functions */
+
+const heroes = ["shaktiman" , "nagaraj" , "doga"]
+let myObj = {
+    name : "Nehal",
+    age : 22
+}
+
+const myFunction = function () { 
+    console.log("hello world");
+}
+    
+
