a9551e4cad98632ac0ddabae5b542e334b6c9d93
NEHAL KHAN <khan.nihal200@gmail.com>
2026-03-25 06:40:50 +0000

    comparison_learning

---
+++ .devcontainer/basics/comparison.js
@@ -0,0 +1,13 @@
+// console.log( 2 >= 1);
+// console.log( 2 > 1);
+// console.log( 2 < 1);
+// console.log( 2 == 1);
+// console.log( 2 != 1);
+
+//Above are normal usage so no need to see those much
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+    
