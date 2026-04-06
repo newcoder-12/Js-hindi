ac7a8b1f4d8742386151e13c3b4f7bc9c59b7ea6
NEHAL KHAN <khan.nihal200@gmail.com>
2026-04-06 06:44:09 +0000

    Object_changes

--- .devcontainer/basics_2/object2.js
+++ .devcontainer/basics_2/object2.js
@@ -10,7 +10,14 @@ tinderUser.name = "harry"
 
 const obj1 = { 1 : "a" , 2 : "b"} 
 const obj2 = { 3 : "c" , 4 : "d"} 
+const obj3 = { 4 : "e" , 6 : "f"} 
 
-const obj3 = { obj1 , obj2 }    // ** problematic situation
+// const obj3 = { obj1 , obj2 }    // ** problematic situation
+//const obj4 = Object.assign({}, obj1 , obj2 , obj3)   // ** useage of object opeartor
 
-console.log(obj3);
+const obj4 = { ...obj1 , ...obj2 , ...obj3}  // ** mostly we use this now days
+
+console.log(obj4);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+ 
